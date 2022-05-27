2bcd611ef8a5c4045d39cd921083865b5bf60f98
greenfructose <justinclarkturney@gmail.com>
2022-05-27 17:07:10 -0500

    Added user repository and service

---
+++ src/main/java/com/justinturney/springuserauthboilerplate/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.justinturney.springuserauthboilerplate.repository;
+
+
+import com.justinturney.springuserauthboilerplate.domain.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/justinturney/springuserauthboilerplate/service/UserService.java
@@ -0,0 +1,22 @@
+package com.justinturney.springuserauthboilerplate.service;
+
+import com.justinturney.springuserauthboilerplate.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND = "";
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(""));
+    }
+}
